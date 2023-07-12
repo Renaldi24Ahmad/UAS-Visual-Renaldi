a59c78ba96258e45df9263f1fd743c093acb72fb
Renaldi24Ahmad <syahrenaldinur@gmail.com>
2023-07-12 23:04:39 +0800

    commit ke 3 (update database)

--- database_siswa.sql
+++ database_siswa.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 29, 2023 at 01:51 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
+-- Waktu pembuatan: 12 Jul 2023 pada 16.56
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_hubungan`
+-- Struktur dari tabel `tabel_hubungan`
 --
 
 CREATE TABLE `tabel_hubungan` (
@@ -37,7 +37,7 @@ CREATE TABLE `tabel_hubungan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tabel_hubungan`
+-- Dumping data untuk tabel `tabel_hubungan`
 --
 
 INSERT INTO `tabel_hubungan` (`id_hub`, `id_siswa`, `id_ortu`, `status_hubungan`, `keterangan`, `status_ortu`) VALUES
@@ -47,7 +47,7 @@ INSERT INTO `tabel_hubungan` (`id_hub`, `id_siswa`, `id_ortu`, `status_hubungan`
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_kelas`
+-- Struktur dari tabel `tabel_kelas`
 --
 
 CREATE TABLE `tabel_kelas` (
@@ -57,17 +57,17 @@ CREATE TABLE `tabel_kelas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tabel_kelas`
+-- Dumping data untuk tabel `tabel_kelas`
 --
 
 INSERT INTO `tabel_kelas` (`id_kelas`, `nama`, `jurusan`) VALUES
 (1, '3 Reg Pagi Bjm', 'IPA'),
-(3, '2X Reg Pagi BJB', 'IPS');
+(3, '2 Reg Pagi BJB', 'IPS');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_ortu`
+-- Struktur dari tabel `tabel_ortu`
 --
 
 CREATE TABLE `tabel_ortu` (
@@ -84,41 +84,91 @@ CREATE TABLE `tabel_ortu` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tabel_ortu`
+-- Dumping data untuk tabel `tabel_ortu`
 --
 
 INSERT INTO `tabel_ortu` (`id_ortu`, `nik`, `nama`, `pendidikan`, `pekerjaan`, `telp`, `alamat`, `jenis_kelamin`, `agama`, `status`) VALUES
-(1, '12345678', 'Alexander Udin', 's2', 'Pensiun', '0895796534681', 'Jl Mars', 'L', 'Islam', 'Hidup'),
-(3, '12323', 'asd', 'asd', 'asd', '2132', 'asda', 'P', 'asd', 'Alm'),
-(4, '12345678', 'Tailor Janah', 's1', 'ceo', '0987654321', 'jl pramuka', 'P', 'islam', 'Hidup');
+(1, '12345678', 'Udin', 'S2', 'Pensiun', '0895796534681', 'Jl Mawar', 'L', 'Islam', 'Hidup'),
+(3, '12323', 'zailani', 'S1', 'Guru', '2132', 'jl.pemurus', 'L', 'islam', 'Hidup'),
+(4, '12345678', 'Janah', 'S1', 'ceo', '08987654321', 'jl pramuka', 'P', 'islam', 'Hidup');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_poin`
+-- Struktur dari tabel `tabel_poin`
 --
 
 CREATE TABLE `tabel_poin` (
   `id_poin` int(10) NOT NULL,
-  `nama_poin` varchar(15) NOT NULL,
+  `nama_poin` varchar(30) NOT NULL,
   `bobot` varchar(10) NOT NULL,
   `jenis` enum('Prestasi','Pelanggaran') NOT NULL,
   `status` enum('True','False') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tabel_poin`
+-- Dumping data untuk tabel `tabel_poin`
 --
 
 INSERT INTO `tabel_poin` (`id_poin`, `nama_poin`, `bobot`, `jenis`, `status`) VALUES
-(1, 'Juara Kelas', '10', 'Prestasi', 'True'),
-(2, 'Juara Dunia', '500', 'Prestasi', 'True'),
-(3, 'Tauran', '50', 'Pelanggaran', 'True');
+(1, 'Siswa Teladan', '35', 'Prestasi', 'True'),
+(2, 'Juara Kelas Per', '20', 'Prestasi', 'True'),
+(3, 'Juara Kelas Peringkat 2', '15', 'Prestasi', 'True'),
+(5, 'Juara Kelas Peringkat 3', '10', 'Prestasi', 'True'),
+(6, 'Juara Umum', '30', 'Prestasi', 'True'),
+(7, 'Juara 1 Lomba Tingkat Kab/Kota', '15', 'Prestasi', 'True'),
+(8, 'Juara 2 Lomba Tingkat Kab/kota', '10', 'Prestasi', 'True'),
+(9, 'Juara 3 Lomba Tingkat Kab/Kota', '5', 'Prestasi', 'True'),
+(10, 'Juara 1 Lomba Tingkat Provinsi', '30', 'Prestasi', 'True'),
+(11, 'Juara 2 Lomba Tingkat Provinsi', '25', 'Prestasi', 'True'),
+(12, 'Juara 3 Lomba Tingkat Provinsi', '20', 'Prestasi', 'True'),
+(13, 'Juara 1 Lomba Tingkat Nasional', '75', 'Prestasi', 'True'),
+(14, 'Juara 2 Lomba Tingkat Nasional', '60', 'Prestasi', 'True'),
+(15, 'Juara 3 Lomba Tingkat Nasional', '45', 'Prestasi', 'True'),
+(16, 'Datang Terlambat', '5', 'Pelanggaran', 'True'),
+(17, 'Keluar Kelas tanpa izin', '5', 'Pelanggaran', 'True'),
+(18, 'Tidak melaksanakan tugas piket', '5', 'Pelanggaran', 'True'),
+(19, 'Tidak berseragam lengkap dan r', '5', 'Pelanggaran', 'True'),
+(20, 'keluar halaman madrasah tanpa ', '10', 'Pelanggaran', 'True'),
+(21, 'melompat pagar atau jendela', '10', 'Pelanggaran', 'True'),
+(22, 'tidak mengikuti sholat berjama', '10', 'Pelanggaran', 'True'),
+(23, 'Siswa berlainan jenis berduaan', '20', 'Pelanggaran', 'True'),
+(24, 'Memalsu raport', '25', 'Pelanggaran', 'True'),
+(25, 'Bergandengan tangan', '20', 'Pelanggaran', 'True'),
+(26, 'Membawa HP/menggunakan pada sa', '20', 'Pelanggaran', 'True'),
+(27, 'Memalsu raport', '25', 'Pelanggaran', 'True'),
+(28, 'Membawa buku/gambar/vidio porn', '25', 'Pelanggaran', 'True'),
+(29, 'Merusak sarana prasarana madra', '25', 'Pelanggaran', 'True'),
+(30, 'Bersikap tidak sopan/ menentan', '25', 'Pelanggaran', 'True'),
+(31, 'Menyinggung guru / karyawan', '25', 'Pelanggaran', 'True'),
+(32, 'Merokok / membawa rokok di dal', '25', 'Pelanggaran', 'True'),
+(33, 'Mengambil barang tanpa izin pe', '25', 'Pelanggaran', 'True'),
+(34, 'Terlibat perkelahian berat (ta', '75', 'Pelanggaran', 'True'),
+(35, 'Membawa minum-minuman keras, M', '75', 'Pelanggaran', 'True'),
+(36, 'Merokok / membawa rokok di dal', '25', 'Pelanggaran', 'True'),
+(37, 'Membawa buku/gambar/vidio porn', '25', 'Pelanggaran', 'True'),
+(38, 'Merusak sarana prasarana madra', '25', 'Pelanggaran', 'True'),
+(39, 'Membawa buku/gambar/vidio porn', '25', 'Pelanggaran', 'True'),
+(40, 'Membawa buku/gambar/vidio porn', '25', 'Pelanggaran', 'True'),
+(41, 'Merusak sarana prasarana madra', '25', 'Pelanggaran', 'True'),
+(42, 'Bersikap tidak sopan/ menentan', '25', 'Pelanggaran', 'True'),
+(43, 'Mengambil barang tanpa izin pe', '25', 'Pelanggaran', 'True'),
+(44, 'Membawa minum-minuman keras, M', '75', 'Pelanggaran', 'True'),
+(45, 'Terlibat penyalahgunaan narkob', '75', 'Pelanggaran', 'True'),
+(46, 'Berurusan dengan pihak berwaji', '75', 'Pelanggaran', 'True'),
+(47, 'Membawa senjata tajam tampa se', '75', 'Pelanggaran', 'True'),
+(48, 'Berjudi di sekolah dan di ling', '75', 'Pelanggaran', 'True'),
+(49, 'Mengancam guru / Karyawan Madr', '75', 'Pelanggaran', 'True'),
+(50, 'Siswa berlainan jenis, berpelu', '75', 'Pelanggaran', 'True'),
+(51, 'Berzinah', '100', 'Pelanggaran', 'True'),
+(52, 'Menikah', '100', 'Pelanggaran', 'True'),
+(53, 'Hamil / menghamili', '100', 'Pelanggaran', 'True'),
+(54, 'Membawa senjata tajam untuk me', '100', 'Pelanggaran', 'True');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_semester`
+-- Struktur dari tabel `tabel_semester`
 --
 
 CREATE TABLE `tabel_semester` (
@@ -135,7 +185,7 @@ CREATE TABLE `tabel_semester` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tabel_semester`
+-- Dumping data untuk tabel `tabel_semester`
 --
 
 INSERT INTO `tabel_semester` (`id_semester`, `id_siswa`, `id_poin`, `id_wali`, `id_ortu`, `id_kelas`, `tanggal`, `semester`, `status`, `tingkat_kelas`) VALUES
@@ -145,7 +195,7 @@ INSERT INTO `tabel_semester` (`id_semester`, `id_siswa`, `id_poin`, `id_wali`, `
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_siswa`
+-- Struktur dari tabel `tabel_siswa`
 --
 
 CREATE TABLE `tabel_siswa` (
@@ -163,18 +213,18 @@ CREATE TABLE `tabel_siswa` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tabel_siswa`
+-- Dumping data untuk tabel `tabel_siswa`
 --
 
 INSERT INTO `tabel_siswa` (`id_siswa`, `nis`, `nisn`, `nama`, `nik`, `tempat_lahir`, `tgl_lahir`, `jenis_kelamin`, `alamat`, `telpon`, `status`) VALUES
-(4, '121', '2110010121', 'Aldinara', '2312313', 'Banjarmasin', '25 Februari 2004', 'L', 'Jl Sungai Andai', '0895806767848', 'aktif'),
-(5, '21321', '3213', 'Perdana', '321312', 'Banajr', '23 Feb 2000', 'L', 'Sungai Jingah', '0824234234', 'aktif'),
-(6, '0987654321', '1234567890', 'Muhammad', '019283234', 'Banjarmasin', '29 Februari 2000', 'L', 'Jl Kayu Tangi', '08978987654', 'aktif');
+(4, '121', '2110010121', 'Lely Humaira', '2312313', 'Banjarmasin', '25 Februari 2004', 'P', 'Jl Sungai Andai', '0895806767848', 'aktif'),
+(5, '21321', '3213', 'Rezaldi', '321312', 'Banjar', '23 Feb 2001', 'L', 'Sungai Jingah', '0824234234', 'aktif'),
+(6, '0987654321', '1234567890', 'Fardani', '019283234', 'Banjarmasin', '20 Februari 2000', 'L', 'Jl Kayu Tangi', '08978987654', 'aktif');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_user`
+-- Struktur dari tabel `tabel_user`
 --
 
 CREATE TABLE `tabel_user` (
@@ -186,17 +236,18 @@ CREATE TABLE `tabel_user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tabel_user`
+-- Dumping data untuk tabel `tabel_user`
 --
 
 INSERT INTO `tabel_user` (`id_user`, `username`, `password`, `level`, `status`) VALUES
 (1, 'user', 'user', 'User', 'Aktif'),
-(2, 'aldinara', 'aldinara123', 'User', 'Aktif');
+(2, 'aldinara', 'aldinara123', 'User', 'Aktif'),
+(4, 'aldi', 'aldi', 'Admin', 'Aktif');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tabel_wali_kelas`
+-- Struktur dari tabel `tabel_wali_kelas`
 --
 
 CREATE TABLE `tabel_wali_kelas` (
@@ -212,7 +263,7 @@ CREATE TABLE `tabel_wali_kelas` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data for table `tabel_wali_kelas`
+-- Dumping data untuk tabel `tabel_wali_kelas`
 --
 
 INSERT INTO `tabel_wali_kelas` (`id_wali`, `nik`, `nama`, `jenis_kelamin`, `pendidikan`, `telp`, `matpel`, `alamat`, `status`) VALUES
@@ -227,7 +278,7 @@ INSERT INTO `tabel_wali_kelas` (`id_wali`, `nik`, `nama`, `jenis_kelamin`, `pend
 --
 
 --
--- Indexes for table `tabel_hubungan`
+-- Indeks untuk tabel `tabel_hubungan`
 --
 ALTER TABLE `tabel_hubungan`
   ADD PRIMARY KEY (`id_hub`),
@@ -237,25 +288,25 @@ ALTER TABLE `tabel_hubungan`
   ADD KEY `id_siswa` (`id_siswa`,`id_ortu`);
 
 --
--- Indexes for table `tabel_kelas`
+-- Indeks untuk tabel `tabel_kelas`
 --
 ALTER TABLE `tabel_kelas`
   ADD PRIMARY KEY (`id_kelas`);
 
 --
--- Indexes for table `tabel_ortu`
+-- Indeks untuk tabel `tabel_ortu`
 --
 ALTER TABLE `tabel_ortu`
   ADD PRIMARY KEY (`id_ortu`);
 
 --
--- Indexes for table `tabel_poin`
+-- Indeks untuk tabel `tabel_poin`
 --
 ALTER TABLE `tabel_poin`
   ADD PRIMARY KEY (`id_poin`);
 
 --
--- Indexes for table `tabel_semester`
+-- Indeks untuk tabel `tabel_semester`
 --
 ALTER TABLE `tabel_semester`
   ADD PRIMARY KEY (`id_semester`),
@@ -268,88 +319,88 @@ ALTER TABLE `tabel_semester`
   ADD KEY `id_kelas` (`id_kelas`);
 
 --
--- Indexes for table `tabel_siswa`
+-- Indeks untuk tabel `tabel_siswa`
 --
 ALTER TABLE `tabel_siswa`
   ADD PRIMARY KEY (`id_siswa`);
 
 --
--- Indexes for table `tabel_user`
+-- Indeks untuk tabel `tabel_user`
 --
 ALTER TABLE `tabel_user`
   ADD PRIMARY KEY (`id_user`);
 
 --
--- Indexes for table `tabel_wali_kelas`
+-- Indeks untuk tabel `tabel_wali_kelas`
 --
 ALTER TABLE `tabel_wali_kelas`
   ADD PRIMARY KEY (`id_wali`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `tabel_hubungan`
+-- AUTO_INCREMENT untuk tabel `tabel_hubungan`
 --
 ALTER TABLE `tabel_hubungan`
   MODIFY `id_hub` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `tabel_kelas`
+-- AUTO_INCREMENT untuk tabel `tabel_kelas`
 --
 ALTER TABLE `tabel_kelas`
   MODIFY `id_kelas` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `tabel_ortu`
+-- AUTO_INCREMENT untuk tabel `tabel_ortu`
 --
 ALTER TABLE `tabel_ortu`
   MODIFY `id_ortu` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `tabel_poin`
+-- AUTO_INCREMENT untuk tabel `tabel_poin`
 --
 ALTER TABLE `tabel_poin`
-  MODIFY `id_poin` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_poin` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
--- AUTO_INCREMENT for table `tabel_semester`
+-- AUTO_INCREMENT untuk tabel `tabel_semester`
 --
 ALTER TABLE `tabel_semester`
   MODIFY `id_semester` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `tabel_siswa`
+-- AUTO_INCREMENT untuk tabel `tabel_siswa`
 --
 ALTER TABLE `tabel_siswa`
   MODIFY `id_siswa` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT for table `tabel_user`
+-- AUTO_INCREMENT untuk tabel `tabel_user`
 --
 ALTER TABLE `tabel_user`
-  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_user` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `tabel_wali_kelas`
+-- AUTO_INCREMENT untuk tabel `tabel_wali_kelas`
 --
 ALTER TABLE `tabel_wali_kelas`
   MODIFY `id_wali` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- Constraints for dumped tables
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Constraints for table `tabel_hubungan`
+-- Ketidakleluasaan untuk tabel `tabel_hubungan`
 --
 ALTER TABLE `tabel_hubungan`
   ADD CONSTRAINT `tabel_hubungan_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `tabel_siswa` (`id_siswa`),
   ADD CONSTRAINT `tabel_hubungan_ibfk_2` FOREIGN KEY (`id_ortu`) REFERENCES `tabel_ortu` (`id_ortu`);
 
 --
--- Constraints for table `tabel_semester`
+-- Ketidakleluasaan untuk tabel `tabel_semester`
 --
 ALTER TABLE `tabel_semester`
   ADD CONSTRAINT `tabel_semester_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `tabel_siswa` (`id_siswa`),
